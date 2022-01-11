092890bdf76c25971ec8f3c595be7ffeae62a905
Luke.Smith <luke.smith@bluewin.ch>
2022-01-11 18:28:44 +0100

    Updates

--- floyd_warshall.py
+++ floyd_warshall.py
@@ -2,9 +2,8 @@ import numpy as np
 from typing import List
 
 def print_latex(ws: List[np.ndarray], bs: List[np.ndarray]):
-    print("\t\\begin{table}[H] \n \
-            \t\\centering \n \
-            \t\\caption{{Floyd-Warshall}}\t")
+    print("\\begin{table}[H]")
+    print("\t\\centering")
 
     for i in range(len(ws)):
         caption = f"Iteration: n = {i}, "
@@ -15,19 +14,20 @@ def print_latex(ws: List[np.ndarray], bs: List[np.ndarray]):
             print_latex_sub_table(ws[i],ws[i-1],True,caption+"Weights")
             print_latex_sub_table(bs[i],bs[i-1],False,caption+"Backtracking")
         if i == 4:
-            print("\t\\end{table} \n \
-                    \t\\pagebreak \n \
-                    \t\\begin{table}[H] \n \
-                    \t\\ContinuedFloat \n \
-                    \t\\centering")
+            print("\t\\caption{{Floyd-Warshall}}")
+            print("\\end{table}")
+            print("\\pagebreak")
+            print("\\begin{table}[H]")
+            print("\t\\ContinuedFloat")
+            print("\t\\centering")
 
     print(f"\t\\caption{{Floyd-Warshall cont.}}")
     print(f"\t\label{{tab:Floyd-Warshall}}")
-    print("\t\\end{table}")
+    print("\\end{table}")
 
 def print_latex_sub_table(t: np.ndarray, t_prev: np.ndarray, weights: bool, caption: str):
-    print("\t\t\\begin{subtable}{0.45\\textwidth} \n \
-          \t\t\\centering")
+    print("\t\t\\begin{subtable}{0.45\\textwidth}")
+    print("\t\t\\centering")
 
     print_latex_tabular(t,t_prev,weights)
 
@@ -36,8 +36,8 @@ def print_latex_sub_table(t: np.ndarray, t_prev: np.ndarray, weights: bool, capt
     print("\t\t\\end{subtable}")
 
 def print_latex_tabular(t: np.ndarray, t_prev: np.ndarray, weights):
-    print("\t\t\t\\begin{tabular}[t]{c ? c | c | c | c | c | c } \n \
-            \t\t\t & \\textbf{a} & \\textbf{b} & \\textbf{c} & \\textbf{d} & \\textbf{e} & \\textbf{f} \\\\")
+    print("\t\t\t\\begin{tabular}[t]{c ? c | c | c | c | c | c }")
+    print("\t\t\t & \\textbf{a} & \\textbf{b} & \\textbf{c} & \\textbf{d} & \\textbf{e} & \\textbf{f} \\\\")
     print("\t\t\t\specialrule{1pt}{0pt}{0pt}")
     rows = ["a","b","c","d","e","f"]
     for i in range(t.shape[0]):
@@ -71,20 +71,28 @@ if __name__=="__main__":
     w = np.array(
         [np.array([np.Inf,1,2,np.Inf,np.Inf,np.Inf]),
         np.array([np.Inf,np.Inf,np.Inf,1,5,np.Inf]),
-        np.array([np.Inf,2,1,3,2,np.Inf]),
+        np.array([np.Inf,2,1,2,3,np.Inf]),
         np.array([4,np.Inf,np.Inf,1,2,4]),
         np.array([np.Inf,np.Inf,np.Inf,np.Inf,10,2]),
         np.array([np.Inf,np.Inf,np.Inf,3,np.Inf,np.Inf])]
     )
 
-    b = np.array(
-        [np.array([np.Inf,0,0,np.Inf,np.Inf,np.Inf]),
-        np.array([np.Inf,np.Inf,np.Inf,1,1,np.Inf]),
-        np.array([np.Inf,2,2,2,2,np.Inf]),
-        np.array([3,np.Inf,np.Inf,3,3,3]),
-        np.array([np.Inf,np.Inf,np.Inf,np.Inf,4,4]),
-        np.array([np.Inf,np.Inf,np.Inf,5,np.Inf,np.Inf])]
-    )
+    # b = np.array(
+    #     [np.array([np.Inf,0,0,np.Inf,np.Inf,np.Inf]),
+    #     np.array([np.Inf,np.Inf,np.Inf,1,1,np.Inf]),
+    #     np.array([np.Inf,2,2,2,2,np.Inf]),
+    #     np.array([3,np.Inf,np.Inf,3,3,3]),
+    #     np.array([np.Inf,np.Inf,np.Inf,np.Inf,4,4]),
+    #     np.array([np.Inf,np.Inf,np.Inf,5,np.Inf,np.Inf])]
+    # )
+    b = np.array([
+        np.array([0,1,2,3,4,5]),
+        np.array([0,1,2,3,4,5]),
+        np.array([0,1,2,3,4,5]),
+        np.array([0,1,2,3,4,5]),
+        np.array([0,1,2,3,4,5]),
+        np.array([0,1,2,3,4,5]),
+    ])
 
     n = w.shape[0]
 
@@ -96,7 +104,7 @@ if __name__=="__main__":
             for j in range(n):
                 if w[i,j] > w[i,k] + w[k,j]:
                     w[i,j] = w[i,k] + w[k,j]
-                    b[i,j] = k
+                    b[i,j] = b[i,k]
         ws.append(np.copy(w))
         bs.append(np.copy(b))
 
